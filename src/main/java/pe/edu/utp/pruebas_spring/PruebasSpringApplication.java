0cd97402772599319ad9c402e885e362aab10718
Andrés León <andres05leonfernandez@gmail.com>
2025-08-25 15:48:22 -0500

    segundo commit pero en intellij

--- src/main/java/pe/edu/utp/pruebas_spring/PruebasSpringApplication.java
+++ src/main/java/pe/edu/utp/pruebas_spring/PruebasSpringApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PruebasSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PruebasSpringApplication.class, args);
+		System.out.println("jose");
+        SpringApplication.run(PruebasSpringApplication.class, args);
 	}
 
 }
