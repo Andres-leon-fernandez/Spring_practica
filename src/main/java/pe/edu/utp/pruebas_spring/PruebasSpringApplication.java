8564d89263740c99d1b2dd25c3e8e52ffbf18966
Andrés León <andres05leonfernandez@gmail.com>
2025-08-25 15:40:25 -0500

    primer commit

---
+++ src/main/java/pe/edu/utp/pruebas_spring/PruebasSpringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.pruebas_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasSpringApplication.class, args);
+	}
+
+}
